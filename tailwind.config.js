a036cf02c7fa5a4cd1a5393dd77ea6d7f78e2dab
Jake Nead <jakenead@gmail.com>
2024-06-13 19:51:49 -0700

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        load: "loadAnimation .5s ease-out forwards",
+      },
+    },
+  },
+  plugins: [],
+};
