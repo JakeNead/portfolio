e1c68fc6a3d2922f6f0c6d7895acec86a93dc48c
Jake Nead <jakenead@gmail.com>
2024-10-29 13:05:10 -0700

    Update relative file paths to use new structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       animation: {
         slideLeft: "slideLeft .5s ease-in-out forwards",
         slideRight: "slideRight .5s ease-in-out forwards",
+      },
+      colors: {
+        blue: "rgb(13, 81, 127)",
+        white: "rgb(255, 255, 255)",
       },
     },
   },
